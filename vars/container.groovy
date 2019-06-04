6a3ddf09e66cae75a1ae3b3c722d68a3e501f1a8
Jan Hellar <jhellar@redhat.com>
2019-06-04 10:39:58 +0200

    name param for container

--- vars/container.groovy
+++ vars/container.groovy
@@ -1,9 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(String image, String network = 'jenkins', Closure body) {
+def call(String image, String name = '', String network = 'jenkins', Closure body) {
+  def nameParam = name == '' ? '' : "--name ${name}"
   docker
     .image(image)
-    .inside("-u root:root --network ${network}") {
+    .inside("-u root:root --network ${network} ${nameParam}") {
       body()
   }
 }
